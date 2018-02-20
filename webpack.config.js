1cdbe80c29e7d53dc2747cf6efe4b667a2569248
Steinar Bang <sb@dod.no>
2018-02-20 21:08:29 +0100

    Initial commit.

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+    entry: './hello.js',
+    output: {
+        filename: 'bundle.js'
+    },
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                query: {
+                    cacheDirectory: true,
+                    presets: ['react']
+                }
+            }
+        ]
+    }
+};
