2972d64d22c284c10931d8b1ab6232d2d79b5890
Steinar Bang <steinar.bang@soprasteria.com>
2018-06-15 22:00:58 +0200

    Implement Component with state, that counts a counter up and down.

--- src/main/frontend/index.js
+++ src/main/frontend/index.js
@@ -1,7 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
+class Counter extends Component {
+    state = { value: 0 };
+
+    countUp() {
+        this.setState({value: this.state.value + 1});
+    }
+
+    countDown() {
+        this.setState({value: this.state.value - 1});
+    }
+
+    render() {
+        return (
+            <p>
+                {this.state.value}
+                <button onClick={() => this.countUp()}>+</button>
+                <button onClick={() => this.countDown()}>-</button>
+            </p>
+        );
+    }
+}
+
 ReactDOM.render(
-    <h1>Hello, world!</h1>,
+    <Counter />,
     document.getElementById('root')
 );
