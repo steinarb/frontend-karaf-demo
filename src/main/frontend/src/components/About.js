c6f6b541093530e66231a03e740c7c4237845594
Steinar Bang <sb@dod.no>
2021-05-09 12:03:00 +0200

    Replace lambda syntax with function syntax for stateless redux components

--- src/main/frontend/src/components/About.js
+++ src/main/frontend/src/components/About.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
-let About = () => (
-    <div>
-        <h1>About the karaf react demo</h1>
+function About() {
+    return (
+        <div>
+            <h1>About the karaf react demo</h1>
 
-        <p>This is a testbed for delivering <a href="https://reactjs.org">react.js</a> frontends from apache karaf.</p>
-    </div>
-);
+            <p>This is a testbed for delivering <a href="https://reactjs.org">react.js</a> frontends from apache karaf.</p>
+        </div>
+    );
+}
 
 export default About;
--- src/main/frontend/src/components/Home.js
+++ src/main/frontend/src/components/Home.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
-let Home = () => (
-    <div>
-        <h1>This is home</h1>
-    </div>
-);
+function Home() {
+    return (
+        <div>
+            <h1>This is home</h1>
+        </div>
+    );
+}
 
 export default Home;
