b9a7b2c1fe83d8ecc0c95aca29d2c112df95ed2b
Steinar Bang <sb@dod.no>
2022-09-01 20:38:43 +0200

    Upgrade react and react-dom to 18.2.0

--- src/main/frontend/src/index.js
+++ src/main/frontend/src/index.js
@@ -1,10 +1,12 @@
 import "core-js/stable";
 import "regenerator-runtime/runtime";
 import React from 'react';
-import { render } from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import App from './components/App';
 
-render(
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+root.render(
     <App />,
-    document.getElementById('root')
 );
